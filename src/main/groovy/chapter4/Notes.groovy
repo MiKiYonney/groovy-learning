071a99c66105bf33e800d43971790efeb90d1348
yonney.yang <yonney.yang@dianping.com>
2015-05-08 16:09:10 +0800

    notes

---
+++ src/main/groovy/chapter4/Notes.groovy
@@ -0,0 +1,13 @@
+package chapter4
+
+/**
+ * Created by yonney.yang on 2015/5/8.
+ */
+class Notes {
+    public static void main(String[] args) {
+        def list = [1,2,3,4]
+        list.each {
+            list.remove(0)
+        }
+    }
+}
