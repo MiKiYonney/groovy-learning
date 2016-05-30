6d9dc9a3856e02df459dd83a0b2d7440016a71d9
yonney.yang <yonney.yang@dianping.com>
2016-05-30 21:37:53 +0800

    add

--- groovy-grammar/src/main/groovy/classics/Collection.groovy
+++ groovy-grammar/src/main/groovy/classics/Collection.groovy
@@ -5,7 +5,7 @@ package classics
  */
 //1. list
 //list.getAt
-def  numbers = [1,2,3]
+def numbers = [1, 2, 3]
 println numbers[0]  //1
 println numbers[3] //null
 println numbers[-1] //3
@@ -19,7 +19,7 @@ println numbers  //[1, 2, 3, 15]
 numbers + [16]
 println numbers //[1, 2, 3, 15]
 
-def num = [23,54,34]
+def num = [23, 54, 34]
 num.add(15)
 println num
 
@@ -27,9 +27,16 @@ num.remove(3)
 println num
 
 //2.映射
+['ken': 'barclay', 'john': 'savage']
+[:]
+def x = 1;
+def y = 2;
+def m = [x: y, y: x]
+println m; //[x:2, y:1]
 
-
-
+//3.范围
+println 10..1 //[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+println 10..<1 //[10, 9, 8, 7, 6, 5, 4, 3, 2]
 
 
 
---
+++ groovy-grammar/src/main/groovy/classics/InAndOut.groovy
@@ -0,0 +1,18 @@
+package classics
+
+/**
+ * Created by yonney on 16/5/30.
+ */
+println "xxx";
+println("xxx")
+
+def staff = ['ken': 222, 'john': 333]
+println "staff is:${staff}"
+
+// 格式化输出
+printf('%d sss %f', [1, 2.0f])
+
+//基本输入
+print "Please enter name:"
+def name = System.in.readLines();
+println "name is:${name}"
